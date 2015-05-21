b176b7bdcb2a4f04384d5382aabeaaf1db34bf6f
Zachary Sells <zacharysells@gmail.com>
2015-05-21 14:54:18 -0700

    Got project into working state. Fixed foriegn key constraints and source code typos.

--- java/src/ProfNetwork.java
+++ java/src/ProfNetwork.java
@@ -40,7 +40,7 @@ public class ProfNetwork {
                                 new InputStreamReader(System.in));
 
    /**
-    * Creates a new instance of Messenger
+    * Creates a new instance of ProfNetwork
     *
     * @param hostname the MySQL or PostgreSQL server hostname
     * @param database the name of the database
@@ -220,6 +220,28 @@ public class ProfNetwork {
       }//end try
    }//end cleanup
 
+
+   public static void FriendList(ProfNetwork esql){
+     //TODO
+     System.out.println("Calling FriendList() function");
+   }
+
+   public static void UpdateProfile(ProfNetwork esql){
+     //TODO
+     System.out.println("Calling UpdateProfile() function");
+   }
+
+   public static void NewMessage(ProfNetwork esql){
+     //TODO
+     System.out.println("Calling NewMessage() function");
+   }
+
+   public static void SendRequest(ProfNetwork esql){
+     //TODO
+     System.out.println("Calling SendRequest() function");
+   }
+
+
    /**
     * The main execution method
     *
@@ -230,22 +252,22 @@ public class ProfNetwork {
          System.err.println (
             "Usage: " +
             "java [-classpath <classpath>] " +
-            Messenger.class.getName () +
+            ProfNetwork.class.getName () +
             " <dbname> <port> <user>");
          return;
       }//end if
 
       Greeting();
-      Messenger esql = null;
+      ProfNetwork esql = null;
       try{
          // use postgres JDBC driver.
          Class.forName ("org.postgresql.Driver").newInstance ();
-         // instantiate the Messenger object and creates a physical
+         // instantiate the ProfNetwork object and creates a physical
          // connection.
          String dbname = args[0];
          String dbport = args[1];
          String user = args[2];
-         esql = new Messenger (dbname, dbport, user, "");
+         esql = new ProfNetwork (dbname, dbport, user, "");
 
          boolean keepon = true;
          while(keepon) {
@@ -331,7 +353,7 @@ public class ProfNetwork {
     * Creates a new user with privided login, passowrd and phoneNum
     * An empty block and contact list would be generated and associated with a user
     **/
-   public static void CreateUser(Messenger esql){
+   public static void CreateUser(ProfNetwork esql){
       try{
          System.out.print("\tEnter user login: ");
          String login = in.readLine();
@@ -341,7 +363,7 @@ public class ProfNetwork {
          String email = in.readLine();
 
 	 //Creating empty contact\block lists for a user
-	 String query = String.format("INSERT INTO USR (userId, password, email, contact_list) VALUES ('%s','%s','%s')", login, password, email);
+	 String query = String.format("INSERT INTO USR (userId, password, email) VALUES ('%s','%s','%s')", login, password, email);
 
          esql.executeUpdate(query);
          System.out.println ("User successfully created!");
@@ -354,7 +376,7 @@ public class ProfNetwork {
     * Check log in credentials for an existing user
     * @return User login or null is the user does not exist
     **/
-   public static String LogIn(Messenger esql){
+   public static String LogIn(ProfNetwork esql){
       try{
          System.out.print("\tEnter user login: ");
          String login = in.readLine();
--- sql/scripts/create_db.sh
+++ sql/scripts/create_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 psql -p $PGPORT $DB_NAME < $DIR/../src/create_tables.sql
-psql -p $PGPORT $DB_NAME < $DIR/../src/create_indexes.sql
+psql -p $PGPORT $DB_NAME < $DIR/../src/create_index.sql
 psql -p $PGPORT $DB_NAME < $DIR/../src/load_data.sql
